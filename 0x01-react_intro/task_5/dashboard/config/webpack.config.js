05ac1d80555942232fe44f12837f63e0e0808572
ChaymaeOuh <chaymae.ouhrouch@edu.uca.ma>
2024-04-26 14:17:44 +0100

    Errors

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -11,26 +11,22 @@ module.exports = {
   module: {
     rules: [
     {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-react'],
-          },
-        },
-      },
+	test: /\.(js|jsx)$/,
+	exclude: /node_modules/,
+	use: ['babel-loader'],
+	},
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: 'file-loader',
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'], 
           },
-        ],
+        },
       },
       {
         test: /\.html$/,
@@ -38,6 +34,10 @@ module.exports = {
       },
     ],
   },
+  resolve: {
+	extensions: ['*', '.js', '.jsx'],
+	},
+	
   plugins: [
     new HtmlWebpackPlugin({
       template: './dist/index.html',
